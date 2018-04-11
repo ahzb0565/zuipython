a36a5bf70de317d12bf49b73e04d8fcbce121352
Bob <ahzb0565@sina.com>
2018-04-11 23:28:17 +0800

    Use axios to get data from backed

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -1,19 +1,47 @@
 <template>
     <div>
-        <center><h3>{{ title }}</h3></center>
-        <vue-markdown>{{ body }}</vue-markdown>
+        <div v-if="error">{{ error }}</div>
+        <div v-else-if="!this.title">Loading ...</div>
+        <div v-else>
+            <center><h3>{{ title }}</h3></center>
+            <div class="article-body">
+                <vue-markdown>{{ body }}</vue-markdown>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 import VueMarkdown from 'vue-markdown'
+import axios from 'axios'
 
 export default {
     name: 'Article',
     data: () => ({
-        title: 'First Article',
-        body: 'i am a ~~tast~~ **test**.'
+        title: null,
+        body: null,
+        id: 1
     }),
-    components: { VueMarkdown }
+    components: { VueMarkdown },
+    created(){
+        if(this.id){
+            axios.get(`/api/article/${this.id}/`)
+                .then((response) => {
+                    console.log(response.data)
+                    this.title = response.data.title
+                    this.body = response.data.body
+                })
+                .catch((error) => {
+                    console.log(error)
+                    this.error = error;
+                })
+        }
+    }
 }
 </script>
+
+<style scoped>
+.article-body {
+    margin: 0px 10px;
+}
+</style>
--- src/components/Catalog.vue
+++ src/components/Catalog.vue
@@ -1,40 +1,41 @@
 <template>
     <div id="sidebar">
-        <b-card title="目录">
-              <b-list-group flush>
-                <b-list-group-item>Windows</b-list-group-item>
-                <b-list-group-item>
-                    <ul>
-                        <li>简介</li>
-                        <li>Excel</li>
-                    </ul>
-                </b-list-group-item>
-              </b-list-group>
-              <b-list-group flush>
-                <b-list-group-item>Windows</b-list-group-item>
-                <b-list-group-item>
-                    <ul>
-                        <li>简介</li>
-                        <li>Excel</li>
-                    </ul>
-                </b-list-group-item>
-              </b-list-group>
-              <b-list-group flush>
-                <b-list-group-item>Windows</b-list-group-item>
-                <b-list-group-item>
-                    <ul>
-                        <li>简介</li>
-                        <li>Excel</li>
-                    </ul>
-                </b-list-group-item>
-              </b-list-group>
+        <b-card title="目录" class="sidebar">
+          <div v-if="error">{{ error }}</div>
+          <div v-if="!topics">Loading...</div>
+          <div v-else>
+            <b-list-group flush v-for="topic in topics">
+              <b-list-group-item>{{ topic.name }}</b-list-group-item>
+              <b-list-group-item>
+                <ul>
+                  <li v-for="article in topic.articles">{{ article }}</li>
+                </ul>
+              </b-list-group-item>
+            </b-list-group>
+          </div>
         </b-card>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-    name: 'Catalog'
+    name: 'Catalog',
+    data: () => ({
+        topics: null,
+        error: null
+    }),
+    created() {
+        axios.get('/api/topics/')
+            .then((response)=> {
+                this.topics = response.data
+            })
+            .catch((error) => {
+                console.log(error)
+                this.error = error
+            })
+    }
 }
 </script>
 
@@ -46,5 +47,9 @@ export default {
     bottom: 0px;
     overflow-y: auto;
     width: 250px;
+}
+
+.sidebar {
+    height: 100%;
 }
 </style>
